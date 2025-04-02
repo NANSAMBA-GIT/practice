e60b08abcc4aa1ef27c6121a83ec30da90b9d4c1
olivia <lwangaolivia20@gmail.com>
2025-04-02 21:17:17 +0300

    fibonnaceforlarge

--- fibonacci.py
+++ fibonacci.py
@@ -20,6 +20,9 @@ class test_fibonacci(unittest.TestCase):
             def test_fibonacci_ten(self):
                 self.assertEqual(fibonnacci(10), 55)
 
+                def test_fibonacci_large(self):
+                    self.assertEqual(fibonnacci(20), 6765)
+
 
 if __name__ == "__main__":
     unittest.main()
