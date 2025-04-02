9b1822e108cf27ea3e2dc5a0f70838c677ad6a1a
olivia <lwangaolivia20@gmail.com>
2025-04-02 20:13:37 +0300

    fibonnaceforzeros

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,18 @@
 import unittest
 
 
+def fibonnacci(n):
+    if n <= 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return (n - 1) + (n - 2)
+
+
 class test_fibonacci(unittest.TestCase):
     def test_fibonacci_zero(self):
-        self.assertEqual(fibonacci(0), 0)
+        self.assertEqual(fibonnacci(0), 0)
 
 
 if __name__ == "__main__":
